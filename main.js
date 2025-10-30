b3c5b8571855d83d5bfce062c98a9cd3f28b9cca
Samuel Tafnes <samuel.tafnes.dias@gmail.com>
2025-10-30 07:34:02 -0300

    atualizado index.html, main,js e style.css

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 $(document).ready(function(){
 
+    const icones = `<ion-icon name="pencil" class="icone" id="editar"></ion-icon>
+                    <ion-icon name="trash" class="icone" id="excluir"></ion-icon>`;
     $('form').on('submit', function(e){
         e.preventDefault();
 
-        const lista_de_tarefas = $('#task_list');
+        const lista_de_tarefas = $('#lista-de-tarefas');
+        const nome_tarefa = $('#nome-tarefa').val();
         const nova_tarefa = document.createElement('li');
+        const label = document.createElement('label');
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
+        checkbox.id = 'check';
 
-        const descricao = document.createElement('span');
-        descricao.textContent = $('#desc_tarefa').val();
+        const tarefa = document.createElement('span');
+        tarefa.className = 'tarefa';
+        tarefa.append(nome_tarefa);
 
-        const btn_excluir = document.createElement('button');
-        btn_excluir.textContent = 'Excluir';
+        const opcoes = document.createElement('span');
+        opcoes.id = 'icones';
+        opcoes.innerHTML = icones;
 
-        nova_tarefa.appendChild(checkbox);
-        nova_tarefa.appendChild(descricao);
-        nova_tarefa.appendChild(btn_excluir);
+        label.append(checkbox);
+        label.append(tarefa);
+        nova_tarefa.append(label);
+        nova_tarefa.append(opcoes);
+        lista_de_tarefas.append(nova_tarefa); 
 
-        lista_de_tarefas.append(nova_tarefa);
-        $('#desc_tarefa').val('');
-
-        btn_excluir.addEventListener('click', () => {
-           nova_tarefa.remove(); // Remove o item da lista
-       });
-    })
+        console.log(nova_tarefa)
+        $('#nome-tarefa').val('');
+    });
 
     
 
 
 
+})
 
-});
